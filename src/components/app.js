5f05a1938a5a782a99cc5ebf02a5393083ca555d
Will Fritts <waf515@gmail.com>
2019-07-05 13:46:45 -0700

    features done

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import '../app.scss';
+import Header from './header.js'
+import Main from './main.js'
+import Footer from './footer.js';
+
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render(){
+    return (
+      <header>Will's TaskMaster</header>
+    )
+  }
+}
+
+export default Header;
