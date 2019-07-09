c85d528a4939273a819c89d21ef550866a1c0f56
Will Fritts <waf515@gmail.com>
2019-07-09 14:16:03 -0700

    stretch goal to make a new task with image

--- src/components/addTask.js
+++ src/components/addTask.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const API = `http://taskmaster-dev-2.us-west-2.elasticbeanstalk.com/`;
+const API = "http://taskmaster-dev-2.us-west-2.elasticbeanstalk.com/"
+// const API =   "http://localhost:5000";
 
 class AddTask extends React.Component{
   render(){
     return(
-      <form action={API} method="post" encType="multipart/form-data">
-        <input type="text" placeholder="Title"></input>
-        <input type="text" placeholder="Description"></input>
-        <input type="text" placeholder="Status"></input>
-        <input type="text" placeholder="Assignee"></input>
-        <input type="file"></input>
-        <input type="submit"></input>
+      <form action={API + "/tasks"} method="post" encType="multipart/form-data">
+        <input type="text" name="title" placeholder="Title"></input>
+        <input type="text" name="description" placeholder="Description"></input>
+        <input type="text" name="status" placeholder="Status"></input>
+        <input type="text" name="assignee" placeholder="Assignee"></input>
+        <input type="file" name="pic"></input>
+        <input type="submit" name="submit"></input>
       </form>
     )
   }
